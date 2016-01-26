620a32baf4509390b9258667de9823c940adbb82
rickykirkendall <rickykirkendall@flocoapps.com>
2016-01-26 11:38:38 -0500

    added comments

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ var testWord = prompt("Enter word here");
 function isPalindrome(word){
 	// Returns true if palindrome
 	var noSpaces = word.replace(/\s/g, '');
+	alert("No Spaces: "+ noSpaces);
 	var cased = noSpaces.toLowerCase();
+	alert("Cased: "+cased);
 	var arr = cased.split("")
+	alert("Arr: "+ arr);
 	var reverse = arr.reverse();
-	var rev_word = reverse.join ("");
+	alert("Reverse: "+reverse);
+	var rev_word = reverse.join("");
+	alert("Rev_word: "+rev_word);
 	return (word == rev_word)
 
 }
